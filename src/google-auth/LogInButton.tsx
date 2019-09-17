00603f5df214174053c971384a4c0f3f7b7fe4eb
akellan <akella.n@gmail.com>
2019-09-17 22:07:37 +0300

    fix: error state

--- src/google-auth/LogInButton.tsx
+++ src/google-auth/LogInButton.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { ObservableComponent, toProps, withEffects } from "refract-rxjs";
-import { from, merge, pipe } from "rxjs";
+import { concat, from, merge, of, pipe, EMPTY } from "rxjs";
 import {
+  catchError,
+  concatMapTo,
   map,
   mapTo,
-  mergeMap,
-  mergeMapTo,
-  retry,
   startWith,
+  switchMap,
+  switchMapTo,
 } from "rxjs/operators";
 import { googleAuthApi } from "./GoogleAuth";
 
@@ -56,8 +57,8 @@ const googleLogInApperture = (
   component: ObservableComponent,
   { googleApi }: IGoogleAuthLogInButtonProps,
 ) => {
-  const mapSignedInInfo = pipe(
-    mergeMapTo(googleApi.getGoogleAuth()),
+  const observeAuthState = pipe(
+    switchMapTo(googleApi.getGoogleAuth()),
     map((googleAuth: gapi.auth2.GoogleAuth) => ({
       isSignedIn: googleAuth.isSignedIn.get(),
       currentUser: googleAuth.currentUser.get(),
@@ -66,7 +67,7 @@ const googleLogInApperture = (
   );
 
   const $isSignedIn = component.mount.pipe(
-    mapSignedInInfo,
+    observeAuthState,
     startWith({
       isSignedIn: false,
       currentUser: null,
@@ -75,29 +76,33 @@ const googleLogInApperture = (
     map(toProps),
   );
 
-  const $logInEventStarted = component
-    .fromEvent("logIn")
-    .pipe(mapTo(toProps({ isLoading: true })));
-
   const $logOutEventStarted = component
     .fromEvent("logOut")
     .pipe(mapTo(toProps({ isLoading: true })));
 
   const $logInEvenToProps = component.fromEvent("logIn").pipe(
-    mergeMapTo(googleApi.getGoogleAuth()),
-    mergeMap((googleAuth: gapi.auth2.GoogleAuth) =>
-      from(googleAuth.signIn({ ux_mode: "popup" })),
+    concatMapTo(
+      concat(
+        of({ isLoading: true }),
+        googleApi.getGoogleAuth().pipe(
+          switchMap((googleAuth: gapi.auth2.GoogleAuth) =>
+            from(googleAuth.signIn({ ux_mode: "popup" })),
+          ),
+          observeAuthState,
+          catchError(() => EMPTY),
+        ),
+        of({ isLoading: false }),
+      ),
     ),
-    mapSignedInInfo,
-    retry(),
-    map((data) => ({ ...data, isLoading: false })),
     map(toProps),
   );
 
   const $logOutEventToProps = component.fromEvent("logOut").pipe(
-    mergeMapTo(googleApi.getGoogleAuth()),
-    mergeMap((googleAuth: gapi.auth2.GoogleAuth) => from(googleAuth.signOut())),
-    mapSignedInInfo,
+    switchMapTo(googleApi.getGoogleAuth()),
+    switchMap((googleAuth: gapi.auth2.GoogleAuth) =>
+      from(googleAuth.signOut()),
+    ),
+    observeAuthState,
     map((data) => ({ ...data, isLoading: false })),
     map(toProps),
   );
@@ -106,7 +111,6 @@ const googleLogInApperture = (
     $isSignedIn,
     $logInEvenToProps,
     $logOutEventToProps,
-    $logInEventStarted,
     $logOutEventStarted,
   );
 };
